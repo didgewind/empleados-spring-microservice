2ef80dd91bc6a1146324f9ccf1d91f24ad4a8c60
made <kike@mazarredos.com>
2019-03-12 11:06:15 +0100

    dependencias de bd eliminadas

--- src/main/java/profe/empleados/negocio/EmpNegocioImpl.java
+++ src/main/java/profe/empleados/negocio/EmpNegocioImpl.java
@@ -5,13 +5,11 @@ import java.util.List;
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import profe.empleados.daos.EmpDAO;
 import profe.empleados.model.Empleado;
 
 @Service
-@Transactional
 public class EmpNegocioImpl implements EmpNegocio {
 
 	@Resource(name="daoMock")
@@ -39,7 +37,6 @@ public class EmpNegocioImpl implements EmpNegocio {
 	}
 
 	@Override
-	@Transactional
 	public void insertaVariosEmpleados(List<Empleado> empleados) {
 		for (Empleado emp : empleados) {
 			dao.insertaEmpleado(emp);
